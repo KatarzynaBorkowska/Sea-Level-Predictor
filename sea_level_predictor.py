148a07cf51d04e6c008c6593ddb496d119c4b5f4
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2023-03-20 18:24:22 +0100

    I done Add labels and title and Finished.

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -31,6 +31,10 @@ def draw_plot():
 
     # Add labels and title
 
+    plt.xlabel("Year")
+    plt.ylabel("Sea Level (inches)")
+    plt.title("Rise in Sea Level")
+
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
